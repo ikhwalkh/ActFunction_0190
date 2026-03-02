8efe751451d9734557c7138539dd9672bdd647e3
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-02 15:29:12 +0700

    first commit

---
+++ Praktikum3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// deklarasi
+int r;
+
+//fungsi dan prosedur
+void input (){
+    cout << "Masukan Jari-Jari Lingkaran : ";
+    cin >> r;
+}
+
+float luaslingkaran (int r){
+    return 3.14159*r*r;
+}
+
+void output(){
+    cout << "Hasilnya : " <<luaslingkaran(r) << endl;
+}
+
+int main (){
+    input ();
+    output ();
+}
